a36407575b15bb06259a70a29790ef894adc8a57
kunal <kunalkumar151123@gmail.com>
2026-01-10 20:25:21 +0530

    Time: 1 ms (99.68%) | Memory: 77.3 MB (50.94%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -11,12 +11,5 @@ class Solution {
         }
         return ans;
     }
-    static {
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-        try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-            fw.write("0");
-        } catch (Exception e) {
-        }
-    }));
-}
+    
 }
