385d9f78696a4275a74a400536a99ac137fc0ec3
kunal <kunalkumar151123@gmail.com>
2026-07-09 09:18:52 +0530

    Time: 2 ms (57.03%) | Memory: 46.5 MB (58.10%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -2,10 +2,10 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] count = new int[26];
 
-    for (final char c : magazine.toCharArray())
+    for ( char c : magazine.toCharArray())
       ++count[c - 'a'];
 
-    for (final char c : ransomNote.toCharArray()) {
+    for ( char c : ransomNote.toCharArray()) {
       if (count[c - 'a'] == 0)
         return false;
       --count[c - 'a'];
