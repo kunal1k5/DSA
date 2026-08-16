7e2ce8bfa47c118ef407c7bec0e57d37a99683ef
kunal <kunalkumar151123@gmail.com>
2026-08-16 15:30:02 +0530

    Time: 1 ms (92.64%) | Memory: 44.4 MB (89.68%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -3,26 +3,32 @@ class Solution {
         return check_Good(nums);
     }
     public static boolean check_Good(int[] arr){
-         Arrays.sort(arr);
-        int n = arr.length;
-        int max = arr[n-1];
-        
-        if(n != max + 1){
-            return false;
-        }
+         int n = arr.length;
+        int max = n-1;
 
-        for(int i =0; i < n - 2; i++){
-            if(arr[i] != i+1){
+        int[] freq = new int[n];
+
+        for(int num : arr){
+            if(num > max || num < 1){
                 return false;
             }
+            freq[num]++;
 
+            // freq[num]++ iska matalab
+            // num = 3 → freq[3]++ → 3 ki count = 1
+            // num = 4 → freq[4]++ → 4 ki count = 1
+            // num = 4 → freq[4]++ → 4 ki count = 2
+            // num = 1 → freq[1]++ → 1 ki count = 1
+            // num = 2 → freq[2]++ → 2 ki count = 1
         }
 
-        if(max != arr[n-2]){
-            return false;
+        for(int i=1; i<max; i++){
+            if(freq[i] != 1){
+                return false;
+            }
         }
 
-        return true;
+        return freq[max] == 2;
     }
 
 }
