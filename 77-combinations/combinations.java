a5ed6453961ce7771a575374baf6a4f42b02a06c
kunal <kunalkumar151123@gmail.com>
2026-08-18 23:44:11 +0530

    Time: 18 ms (79.84%) | Memory: 98 MB (46.74%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -2,26 +2,22 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<Integer> ll = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        boolean[] board = new boolean[n];
+      
 
-        combination(board, k, 0,ll, ans,0);
+        combination(n, k, 1,ll, ans);
         return ans;
     }
-     public static void combination(boolean[] board, int tq,int qpsf, List<Integer> ll,List<List<Integer>> ans,int idx){
+     public static void combination(int n, int k,int idx, List<Integer> ll,List<List<Integer>> ans){
 
-        if(tq == qpsf){
+        if(ll.size() == k){
            // System.out.println(ans);
             ans.add(new ArrayList<>(ll));
             return;
         }
-        for(int i=idx; i<board.length; i++){
-            if(board[i] == false){ // box khali hai
-                board[i] = true;// queen baith gyi
-                ll.add(i+1);
-                combination(board, tq, qpsf+1,ll, ans,i + 1);
+        for(int i=idx; i<=n; i++){
+                ll.add(i);
+                combination(n, k, i+1,ll, ans);
                 ll.remove(ll.size()-1);
-                board[i] = false;// queen nikal gyi
-            }
         }
     }
     
